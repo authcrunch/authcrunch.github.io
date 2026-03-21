e5dfd5657bf158140847a7796136c7be5e6622b1
Paul Greenberg <greenpau@outlook.com>
2026-03-21 04:51:58 -0400

    docusaurus: update header

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,44 +1,43 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   title: "AuthCrunch aka Caddy Security Documentation",
   tagline: "Security App for Caddy v2",
-  favicon: 'img/favicon.ico',
+  favicon: "img/favicon.ico",
 
   url: "https://docs.authcrunch.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
   markdown: {
-    format: 'mdx',
+    format: "mdx",
     hooks: {
-      onBrokenMarkdownLinks: 'warn',
-    }
+      onBrokenMarkdownLinks: "warn",
+    },
   },
   organizationName: "authcrunch",
   projectName: "authcrunch.github.io",
   trailingSlash: false,
 
-
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
-  
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         googleAnalytics: {
           trackingID: "G-Q4KYV782E8",
@@ -47,22 +46,24 @@ const config: Config = {
           trackingID: "G-Q4KYV782E8",
         },
         docs: {
-          sidebarPath: './sidebars.ts',
-          editUrl: 'https://github.com/authcrunch/authcrunch.github.io/edit/main/',
+          sidebarPath: "./sidebars.ts",
+          editUrl:
+            "https://github.com/authcrunch/authcrunch.github.io/edit/main/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
-          editUrl: 'https://github.com/authcrunch/authcrunch.github.io/edit/main/',
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          editUrl:
+            "https://github.com/authcrunch/authcrunch.github.io/edit/main/",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
         sitemap: {
           changefreq: "weekly",
@@ -73,7 +74,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    image: 'img/authcrunch.jpg',
+    image: "img/authcrunch.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
@@ -97,28 +98,31 @@ const config: Config = {
           label: "Authorize",
         },
         {
-          type: "doc",
-          docId: "credentials/intro",
+          type: "dropdown",
+          label: "Other",
           position: "left",
-          label: "Credentials",
-        },
-        {
-          type: "doc",
-          docId: "messaging/intro",
-          position: "left",
-          label: "Messaging",
-        },
-        {
-          type: "doc",
-          docId: "apps/intro",
-          position: "left",
-          label: "Apps",
-        },
-        {
-          type: "doc",
-          docId: "examples/intro",
-          position: "left",
-          label: "Examples",
+          items: [
+            {
+              type: "doc",
+              docId: "credentials/intro",
+              label: "Credentials",
+            },
+            {
+              type: "doc",
+              docId: "messaging/intro",
+              label: "Messaging",
+            },
+            {
+              type: "doc",
+              docId: "apps/intro",
+              label: "Apps",
+            },
+            {
+              type: "doc",
+              docId: "examples/intro",
+              label: "Examples",
+            },
+          ],
         },
         {
           href: "https://github.com/greenpau/caddy-security/issues/new/choose",
@@ -129,7 +133,7 @@ const config: Config = {
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
           title: "Documentation",
